37547f9ea59b87e0c8c73154c877400d26d97d76
Ehimah Obuse <ehimahobuse@gmail.com>
2019-01-12 21:18:21 +0100

    Added CarAdvert Model and FuelTypes

---
+++ CarAdverts/Domain/Entity/CarAdvert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarAdverts.Domain.Entity
+{
+    public class CarAdvert
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public FuelType Fuel { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public bool New { get; set; }
+        public int Mileage { get; set; }
+        [Column(TypeName ="date")]
+        public DateTime FirstRegistration { get; set; }
+    }
+}
