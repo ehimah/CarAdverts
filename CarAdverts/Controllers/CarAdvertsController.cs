5a0e09aa49a851a5611b7c7c69a1a9ad2ee0810d
Ehimah Obuse <ehimahobuse@gmail.com>
2019-01-12 14:45:56 +0100

    Initial Commit - Barebones Project setup

---
+++ CarAdverts/Controllers/CarAdvertsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarAdverts.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarAdverts.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/CarAdverts")]
+    public class CarAdvertsController : Controller
+    {
+        // GET: api/CarAdverts
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return NotFound();
+        }
+
+        // GET: api/CarAdverts/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult Get(int id, [FromQuery] CarAdvertQueryModel carAdveryQuery = null)
+        {
+            return NoContent();
+        }
+        
+        // POST: api/CarAdverts
+        [HttpPost]
+        public ActionResult Post([FromBody]CarAdvertRequestModel value)
+        {
+            return CreatedAtAction("Get",value);
+        }
+        
+        // PUT: api/CarAdverts/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, CarAdvertRequestModel value)
+        {
+            return NoContent();
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
